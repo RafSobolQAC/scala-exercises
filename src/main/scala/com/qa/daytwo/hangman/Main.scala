f5e51e4981020536e4278d6a76d1388aa5be845e
Raf Sobol <rafal.sobol@academytrainee.com>
2020-03-04 09:36:25 +0000

    Hangman graphics complete.

--- src/main/scala/com/qa/daytwo/hangman/Main.scala
+++ src/main/scala/com/qa/daytwo/hangman/Main.scala
@@ -23,8 +23,42 @@ object Main extends App {
     }
 
   }
-
-
+  def hangmanSetup(hangman: scala.collection.mutable.ListBuffer[String], wrongs: scala.collection.mutable.HashSet[Char]) = {
+    val leg1= " |"
+    val leg2 = " |   |"
+    val base = "_______"
+    val stan = "   |"
+    val topy = " ____________"
+    val rope = "   |    |"
+    val head = "   |    o"
+    val body = "   |    |"
+    val arml = "   |   /|"
+    val arms = "   |   /|\\"
+    val legl = "   |   /"
+    val dead = "   |   / \\"
+    wrongs.size match {
+      case 1 => hangman += leg1
+      case 2 => hangman(0) = leg2
+      case 3 => base +=: hangman
+      case 4 => for (i <- 1 to 6) stan +=: hangman
+      case 5 => topy +=: hangman
+      case 6 => hangman(1) = rope
+      case 7 => hangman(2) = head
+      case 8 => hangman(3) = body
+      case 9 => hangman(3) = arml
+      case 10 => hangman(3) = arms
+      case 11 => hangman(4) = legl
+      case 12 => hangman(4) = dead
+      case _ =>
+    }
+    hangman
+  }
+  def printHangman(hangman: scala.collection.mutable.ListBuffer[String]): Unit = {
+    if (hangman.length < 9) {
+      for (i <- 1 to (9 - hangman.length)) println()
+    }
+    hangman.foreach(println)
+  }
   def wrongGuess(c: Char, wrongs: scala.collection.mutable.HashSet[Char]) = {
     wrongs.add(c)
     println(wrongs.size + " wrongs so far! " + (5 - wrongs.size) + " to go!")
@@ -48,14 +82,20 @@ object Main extends App {
 
   def askToGuess(): Char = {
     println("Guess a character!")
-    val c = scala.io.StdIn.readChar()
-    c
+    var c = ' '
+    while (!c.isLetter) {
+      println("Please only input one letter.")
+      val test = scala.io.StdIn.readLine()
+      if (!test.isEmpty && test.length == 1) c = test.charAt(0)
+    }
+    c.toLower
   }
 
   def runGame(goal: String): Boolean = {
     var wrongs = new scala.collection.mutable.HashSet[Char]
     var current = new scala.collection.mutable.ListBuffer[Char]
     var view = new scala.collection.mutable.ListBuffer[Char]
+    var hangman = new scala.collection.mutable.ListBuffer[String]
     for (i <- 0 until goal.length) {
       view.addOne('_')
     }
@@ -63,7 +103,7 @@ object Main extends App {
     view.foreach(el => print(el + " "))
 
     do {
-      for (i <- 1 to 5) println()
+      for (i <- 1 to 3) println()
       var break = true
       while (break) {
         c = askToGuess()
@@ -75,19 +115,22 @@ object Main extends App {
       }
       else {
         wrongs = wrongGuess(c, wrongs)
+        hangman = hangmanSetup(hangman, wrongs)
       }
+      printHangman(hangman)
       view.foreach(el => print(el + " "))
       println()
       print("Guessed wrong: ")
       wrongs.foreach(el => print(el + " "))
 
-    } while (view.contains('_') && wrongs.size < 5)
+    } while (view.contains('_') && wrongs.size < 12)
 
-    if (wrongs.size == 5) false else true
+    if (wrongs.size == 12) false else true
 
   }
+
   def getWord(diff: Int): String = {
-    val source = Source.fromFile("/home/qa-admin/enable1.txt")
+    val source = Source.fromFile("/home/qa-admin/words.txt")
     val random = scala.util.Random
     var lines = source.getLines().toList
     val word = diff match {
@@ -95,19 +138,20 @@ object Main extends App {
         lines = lines.filter(el => el.length >= 10)
         lines((random.nextFloat() * lines.size).toInt)
       }
-      case 2 =>  {
+      case 2 => {
         lines = lines.filter(el => el.length >= 6 && el.length < 10)
-        lines((random.nextFloat () * lines.size).toInt)
+        lines((random.nextFloat() * lines.size).toInt)
       }
       case 3 => {
         lines = lines.filter(el => el.length < 6)
-        lines((random.nextFloat () * lines.size).toInt)
+        lines((random.nextFloat() * lines.size).toInt)
       }
 
     }
     word
 
   }
+
   def pickDifficulty(): Int = {
     println("(E)asy, (m)edium or (h)ard?")
     val input = scala.io.StdIn.readChar()
@@ -119,11 +163,14 @@ object Main extends App {
     }
     diff
   }
-  def playAgain : Boolean = {
+
+  def playAgain: Boolean = {
     println("Play again? (y/n)")
     val yesNo = scala.io.StdIn.readChar()
     if (yesNo == 'y') true else false
   }
+
+
   def play(): Unit = {
     do {
       val goal = getWord(pickDifficulty())
@@ -140,7 +187,7 @@ object Main extends App {
         println("The word was " + goal)
       }
 
-      for (i<-1 to 30) print("-")
+      for (i <- 1 to 30) print("-")
       println()
     } while (playAgain)
   }
